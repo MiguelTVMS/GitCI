a18ef8faec5966b51d36936d58c88722ba4d8dbf
João Miguel <joao.miguel@gruposbf.com.br>
2016-07-06 21:18:59 -0300

    Created the functions to get the available modules, scripts to execute and must continue the modules validation

--- test/index.js
+++ test/index.js
@@ -1,16 +1,2 @@
 'use strict';
 
-const forever = require('forever-monitor');
-
-var child = new (forever.Monitor)('lib/index.js', {
-    max: 1,
-    silent: false,
-    args: []
-});
-
-child.on('exit', function () {
-    console.log('GitCI has ended.');
-    process.exit(0);
-});
-
-child.start();
